23157951bb12057dd21e86f0f61c5e692328bc7a
AisyahHumairah <aisyah.ahumz@gmail.com>
2019-12-23 10:24:06 +0700

    PunyaRoyUmayFirebaseRxMVP

---
+++ app/src/main/java/ch/berco/loginmvvmfirebase/ui/auth/SignupActivity.kt
@@ -0,0 +1,57 @@
+package ch.berco.loginmvvmfirebase.ui.auth
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProviders
+import ch.berco.loginmvvmfirebase.R
+import ch.berco.loginmvvmfirebase.databinding.ActivitySignupBinding
+import ch.berco.loginmvvmfirebase.utils.startIsiBiodataActivity
+import kotlinx.android.synthetic.main.activity_signup.*
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.kodein
+import org.kodein.di.generic.instance
+
+class SignupActivity : AppCompatActivity(), AuthListener, KodeinAware {
+
+    override val kodein by kodein()
+    private val factory : AuthViewModelFactory by instance()
+
+    private lateinit var viewModel: AuthViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+
+        val binding: ActivitySignupBinding = DataBindingUtil.setContentView(this, R.layout.activity_signup)
+        viewModel = ViewModelProviders.of(this, factory).get(AuthViewModel::class.java)
+        binding.viewmodel = viewModel
+
+        viewModel.authListener = this
+    }
+
+    override fun onSuccessAdmin() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onStarted() {
+        progressbar.visibility = View.VISIBLE
+        Intent(this, LoginActivity::class.java).also {
+            it.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(it)
+        }
+    }
+
+    override fun onSuccess() {
+        progressbar.visibility = View.GONE
+        startIsiBiodataActivity()
+    }
+
+    override fun onFailure(message: String) {
+        progressbar.visibility = View.GONE
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+}
